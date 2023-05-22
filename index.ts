c984615776e44c1f9450db06ffdf948d2e31ad82
cauapdebiasi <72683559+cauapdebiasi@users.noreply.github.com>
2023-05-22 19:31:15 -0300

    1 file modified

--- index.ts
+++ index.ts
@@ -1,4 +1,6 @@
 class Veiculo {
+  infracoes: { velocidade: number }[] = [];
+
   constructor(private placa: string, private velocidade: number) {}
 
   getPlaca(): string {
@@ -8,13 +10,24 @@ class Veiculo {
   getVelocidade(): number {
     return this.velocidade;
   }
+
+  adicionarInfracao(velocidade: number): void {
+    this.infracoes.push({ velocidade });
+  }
+
+  mostrarInfracoes(): void {
+    console.log("Infrações registradas para o veículo de placa", this.placa);
+    for (const infracao of this.infracoes) {
+      console.log("Velocidade:", infracao.velocidade);
+    }
+  }
 }
 
 class Radar {
   static readonly LIMITE_VELOCIDADE_CARRO_MOTO = 100;
   static readonly LIMITE_VELOCIDADE_CAMINHAO = 100;
   static readonly TOLERANCIA_INFRACAO = 0.05;
-  static infracoes: { placa: string; velocidade: number }[] = [];
+  static infracoes: { placa: string, velocidade: number }[] = [];
 
   static verificarVelocidade(veiculo: Veiculo): void {
     let limiteVelocidade: number;
@@ -23,7 +36,7 @@ class Radar {
     } else if (veiculo instanceof Caminhao) {
       limiteVelocidade = Radar.LIMITE_VELOCIDADE_CAMINHAO;
     } else {
-      throw new Error('Tipo de veículo inválido.');
+      throw new Error("Tipo de veículo inválido.");
     }
 
     const velocidadeMaxima = limiteVelocidade * (1 + Radar.TOLERANCIA_INFRACAO);
@@ -35,20 +48,17 @@ class Radar {
   private static notificarInfracao(veiculo: Veiculo): void {
     const infracao = {
       placa: veiculo.getPlaca(),
-      velocidade: veiculo.getVelocidade(),
+      velocidade: veiculo.getVelocidade()
     };
     this.infracoes.push(infracao);
-    console.log(
-      `Infração registrada - Placa: ${infracao.placa}, Velocidade: ${infracao.velocidade}`
-    );
+    veiculo.adicionarInfracao(infracao.velocidade);
+    console.log(`Infração registrada - Placa: ${infracao.placa}, Velocidade: ${infracao.velocidade}`);
   }
 
-  static mostrarInfracoes(): void {
-    console.log('Infrações registradas:');
+  static mostrarInfracoesRegistradas(): void {
+    console.log("Infrações registradas:");
     for (const infracao of this.infracoes) {
-      console.log(
-        `Placa: ${infracao.placa}, Velocidade: ${infracao.velocidade}`
-      );
+      console.log(`Placa: ${infracao.placa}, Velocidade: ${infracao.velocidade}`);
     }
   }
 }
@@ -80,7 +90,16 @@ Radar.verificarVelocidade(carro); // Irá notificar a infração e registrar
 Radar.verificarVelocidade(caminhao); // Não irá notificar a infração
 Radar.verificarVelocidade(moto); // Não Irá notificar a infração e registrar
 
-Radar.mostrarInfracoes(); // Mostra as infrações registradas
+carro.mostrarInfracoes(); // Mostra as infrações registradas para o motora
+/* 
+[{
+  Velocidade: 106
+}]
+  */
+caminhao.mostrarInfracoes(); // Mostra as infrações registradas para o veículo
+moto.mostrarInfracoes(); // Mostra as infrações registradas para o veículo
+
+Radar.mostrarInfracoesRegistradas();
 /* 
 Infrações registradas:
 Placa: DEF789, Velocidade: 106
